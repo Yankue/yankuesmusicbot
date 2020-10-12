6a2dea92becc8c1b27bc5ca1eca9e47b9c773236
Yankue <tpower@highstorrs.sheffield.sch.uk>
2020-10-12 12:36:46 +0100

    Uploaded

---
+++ commands/stop.js
@@ -0,0 +1,18 @@
+﻿const { canModifyQueue } = require("../util/handler");
+
+
+module.exports = {
+    name: "destroy",
+    aliases: ["stop", "fs"],
+    description: "Stops the music",
+    execute(message) {
+        const queue = message.client.queue.get(message.guild.id);
+
+        if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+        if (!canModifyQueue(message.member)) return;
+
+        queue.songs = [];
+        queue.connection.dispatcher.end();
+        queue.textChannel.send(`${message.author} stopped the music!`).catch(console.error);
+    }
+};
---
+++ include/play.js
@@ -0,0 +1,92 @@
+﻿const ytdlDiscord = require("ytdl-core-discord");
+const scdl = require("soundcloud-downloader");
+const { canModifyQueue } = require("../util/handler");
+
+module.exports = {
+    async play(song, message) {
+        const { PRUNING, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
+        const queue = message.client.queue.get(message.guild.id);
+
+        if (!song) {
+            queue.channel.leave();
+            message.client.queue.delete(message.guild.id);
+            return queue.textChannel.send("Music queue ended.").catch(console.error);
+        }
+
+        let stream = null;
+        let streamType = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
+
+        try {
+            if (song.url.includes("youtube.com")) {
+                stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
+            } else if (song.url.includes("soundcloud.com")) {
+                try {
+                    stream = await scdl.downloadFormat(
+                        song.url,
+                        scdl.FORMATS.OPUS,
+                        SOUNDCLOUD_CLIENT_ID ? SOUNDCLOUD_CLIENT_ID : undefined
+                    );
+                } catch (error) {
+                    stream = await scdl.downloadFormat(
+                        song.url,
+                        scdl.FORMATS.MP3,
+                        SOUNDCLOUD_CLIENT_ID ? SOUNDCLOUD_CLIENT_ID : undefined
+                    );
+                    streamType = "unknown";
+                }
+            }
+        } catch (error) {
+            if (queue) {
+                queue.songs.shift();
+                module.exports.play(queue.songs[0], message);
+            }
+
+            console.error(error);
+            return message.channel.send(`Error: ${error.message ? error.message : error}`);
+        }
+
+        queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
+
+        const dispatcher = queue.connection
+            .play(stream, { type: streamType })
+            .on("finish", () => {
+                if (collector && !collector.ended) collector.stop();
+
+                if (queue.loop) {
+                    // if loop is on, push the song back at the end of the queue
+                    // so it can repeat endlessly
+                    let lastSong = queue.songs.shift();
+                    queue.songs.push(lastSong);
+                    module.exports.play(queue.songs[0], message);
+                } else {
+                    // Recursively play the next song
+                    queue.songs.shift();
+                    module.exports.play(queue.songs[0], message);
+                }
+            })
+            .on("error", (err) => {
+                console.error(err);
+                queue.songs.shift();
+                module.exports.play(queue.songs[0], message);
+            });
+        dispatcher.setVolumeLogarithmic(queue.volume / 100);
+
+        try {
+            var playingMessage = await queue.textChannel.send(`🎶 Started playing: **${song.title}** ${song.url}`);
+        } catch (error) {
+            console.error(error);
+        }
+
+        const filter = (reaction, user) => user.id !== message.client.user.id;
+        var collector = playingMessage.createReactionCollector(filter, {
+            time: song.duration > 0 ? song.duration * 1000 : 600000
+        });
+
+        collector.on("end", () => {
+            playingMessage.reactions.removeAll().catch(console.error);
+            if (PRUNING && playingMessage && !playingMessage.deleted) {
+                playingMessage.delete({ timeout: 3000 }).catch(console.error);
+            }
+        });
+    }
+};
