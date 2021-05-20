2573d36baa5ed79d8c2013f6884d84972d04a6ac
SinisterDeveloper <80232412+SinisterDeveloper@users.noreply.github.com>
2021-05-20 18:04:07 +0530

    Added embeds

--- commands/stop.js
+++ commands/stop.js
@@ -1,5 +1,5 @@
-﻿const { canModifyQueue } = require("../util/handler");
-
+const { canModifyQueue } = require("../util/handler");
+const Discord = require(`discord.js`);
 
 module.exports = {
     name: "destroy",
@@ -8,11 +8,24 @@ module.exports = {
     execute(message) {
         const queue = message.client.queue.get(message.guild.id);
 
-        if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+        let NothingPlaying = new Discord.MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle(`Nothing Playing!`)
+            .setDescription(`The queue is empty!`);
+
+        if (!queue) return message.reply(NothingPlaying).catch(console.error);
+
         if (!canModifyQueue(message.member)) return;
 
         queue.songs = [];
         queue.connection.dispatcher.end();
-        queue.textChannel.send(`${message.author} stopped the music!`).catch(console.error);
+        let StoppedEmbed = new Discord.MessageEmbed()
+            .setColor("#3b78e0")
+            .setTitle("Player Stopped")
+            .setThumbnail(`https://cdn.discordapp.com/emojis/830034712650317825.png?v=1`)
+            .setDescription(`The player has been stopped `)
+            .addField(`Stopped by`, message.author);
+
+        queue.textChannel.send(StoppedEmbed).catch(console.error);
     }
 };
