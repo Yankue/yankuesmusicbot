e82806891236d10d75da8309e458af0c2390e551
SinisterDeveloper <80232412+SinisterDeveloper@users.noreply.github.com>
2021-05-20 17:55:02 +0530

    Embedded

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,5 @@
 ﻿const { canModifyQueue } = require("../util/handler");
+const Discord = require(`discord.js`);
 
 module.exports = {
     name: "skip",
@@ -6,12 +7,25 @@ module.exports = {
     description: "Skip the currently playing song",
     execute(message) {
         const queue = message.client.queue.get(message.guild.id);
+
+        let NothingPlaying = new Discord.MessageEmbed()
+            .setColor("#ff0000")
+            .setTitle(`Nothing Playing`)
+            .setDescription(`The queue is empty!`);
+
+
         if (!queue)
-            return message.reply("There is nothing playing that I could skip for you.").catch(console.error);
+            return message.reply(NothingPlaying).catch(console.error);
         if (!canModifyQueue(message.member)) return;
+
+        let SkippedEmbed = new Discord.MessageEmbed()
+            .setColor("#3b78e0")
+            .setTitle("Song Skipped")
+            .setDescription(`The song has been skipped`)
+            .addField(`Skipped by`, message.author);
 
         queue.playing = true;
         queue.connection.dispatcher.end();
-        queue.textChannel.send(`${message.author} ⏭ skipped the song`).catch(console.error);
+        queue.textChannel.send(SkippedEmbed).catch(console.error);
     }
 };
