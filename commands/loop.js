43cc0ee21f2087864766c3208737f5235360b271
SinisterDeveloper <80232412+SinisterDeveloper@users.noreply.github.com>
2021-05-20 17:12:50 +0530

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -13,7 +13,7 @@ module.exports = {
 
         
         const queue = message.client.queue.get(message.guild.id);
-        if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+        if (!queue) return message.reply(NothingPlaying).catch(console.error);
         if (!canModifyQueue(message.member)) return;
 
         // toggle from false to true and reverse
